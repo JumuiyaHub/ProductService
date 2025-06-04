dbe617ec68375a227933b1d37ee93e13fe871337
KiprutoKelvin <127014004+KiprutoKelvin@users.noreply.github.com>
2025-06-04 20:42:40 +0300

    feat: implement basic Product REST API endpoints

---
+++ src/main/java/com/kiru/microservice/product/controller/ProductController.java
@@ -0,0 +1,30 @@
+package com.kiru.microservice.product.controller;
+
+import com.kiru.microservice.product.dto.ProductRequest;
+import com.kiru.microservice.product.dto.ProductResponse;
+import com.kiru.microservice.product.model.Product;
+import com.kiru.microservice.product.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/product")
+@RequiredArgsConstructor
+public class ProductController {
+    private final ProductService productService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ProductResponse createProduct(@RequestBody ProductRequest productRequest) {
+        return productService.createProduct(productRequest);
+    }
+
+    @GetMapping()
+    @ResponseStatus(HttpStatus.OK)
+    public List<ProductResponse> getAllProducts() {
+        return productService.getAllProducts();
+    }
+}
