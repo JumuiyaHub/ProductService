21e690f7c5eac4001e66e4c3c55af3d4133d5852
KiprutoKelvin <127014004+KiprutoKelvin@users.noreply.github.com>
2025-06-04 20:52:58 +0300

    feat: add Product MongoDB entity with core fields

---
+++ src/main/java/com/kiru/microservice/product/model/Product.java
@@ -0,0 +1,26 @@
+package com.kiru.microservice.product.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Document(value = "product")
+public class Product {
+    @Id
+    private String id;
+    private String name;
+    private String description;
+    private BigDecimal price;
+
+
+    // Additional fields can be added as needed
+}
