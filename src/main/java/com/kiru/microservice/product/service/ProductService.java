11b5456a8fbd250c340cd3685af38e38655e9569
KiprutoKelvin <127014004+KiprutoKelvin@users.noreply.github.com>
2025-06-05 18:38:00 +0300

    feat: implement ProductService with create and retrieve all products functionality

---
+++ src/main/java/com/kiru/microservice/product/service/ProductService.java
@@ -0,0 +1,42 @@
+package com.kiru.microservice.product.service;
+
+import com.kiru.microservice.product.dto.ProductRequest;
+import com.kiru.microservice.product.dto.ProductResponse;
+import com.kiru.microservice.product.model.Product;
+import com.kiru.microservice.product.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class ProductService {
+    private final ProductRepository productRepository;
+    public ProductResponse createProduct(ProductRequest productRequest) {
+        Product product = Product.builder()
+                .name(productRequest.name())
+                .description(productRequest.description())
+                .price(productRequest.price())
+                .build();
+        productRepository.save(product);
+        log.info("Product created successfully");
+        return new ProductResponse(
+                product.getId(),
+                product.getName(),
+                product.getDescription(),
+                product.getPrice());
+    }
+    public List<ProductResponse> getAllProducts() {
+        return productRepository.findAll()
+                .stream()
+                .map(product -> new ProductResponse(
+                        product.getId(),
+                        product.getName(),
+                        product.getDescription(),
+                        product.getPrice()))
+                .toList();
+    }
+}
